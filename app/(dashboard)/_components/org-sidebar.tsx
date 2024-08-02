3a27df89c7ade50abd9ffdd64e1e48b21a01b91a
Oleg Vrabiy <olegvraby@gmail.com>
2024-08-02 18:28:39 +0200

    create: org-sidebar.tsx: initial markup

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const OrgSidebar = () => {
+  return (
+    <div className="hidden w-[206px] flex-col space-y-6 bg-red-400 pl-5 pt-5 lg:flex">
+      OrgSidebar
+    </div>
+  );
+};
+
+export default OrgSidebar;
