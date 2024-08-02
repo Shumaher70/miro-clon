4c5d8f082b1acc98588860a844b588f692fce16d
Oleg Vrabiy <olegvraby@gmail.com>
2024-08-02 17:56:51 +0200

    add:page.tsx: initial markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for authenticated users only </div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
