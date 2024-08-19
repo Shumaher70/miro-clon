4ee17d903f1fa635cc99543f2d3e957827fe25ad
Oleg Vrabiy <olegvraby@gmail.com>
2024-08-19 18:30:40 +0200

    add:toolbar.tsx: ToolButton component has been added instead of dummy "DIV"s

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,16 +1,70 @@
-import React from "react";
+import {
+  Type,
+  Undo2,
+  Redo2,
+  Pencil,
+  Circle,
+  Square,
+  StickyNote,
+  MousePointer2,
+} from "lucide-react";
+
+import ToolButton from "./tool-button";
 
 const Toolbar = () => {
   return (
     <div className="absolute left-2 top-1/2 flex -translate-y-1/2 flex-col gap-y-4">
       <div className="flex flex-col items-center gap-1 rounded-md bg-white p-1.5 shadow-md">
-        <div>Pencil</div>
-        <div>Pencil</div>
-        <div>Pencil</div>
+        <ToolButton
+          label="Select"
+          icon={MousePointer2}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Text"
+          icon={Type}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Sticky note"
+          icon={StickyNote}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Rectangle"
+          icon={Square}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Ellipse"
+          icon={Circle}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Pen"
+          icon={Pencil}
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
       <div className="flex flex-col items-center rounded-md bg-white p-1.5 shadow-md">
-        <div>Undo</div>
-        <div>Redo</div>
+        <ToolButton
+          label="Undo"
+          icon={Undo2}
+          onClick={() => {}}
+          isDisabled={true}
+        />
+        <ToolButton
+          label="Redo"
+          icon={Redo2}
+          onClick={() => {}}
+          isDisabled={true}
+        />
       </div>
     </div>
   );
