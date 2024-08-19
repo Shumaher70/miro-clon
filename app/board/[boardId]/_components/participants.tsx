181287c4a28ec2a83d6aa443c5a97526740a5b76
Oleg Vrabiy <olegvraby@gmail.com>
2024-08-19 18:10:06 +0200

    add:participants.tsx:  userAvatar component to display user avatars with Liveblocks integration

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+"use client";
+import { useOthers, useSelf } from "@liveblocks/react/suspense";
+import { connectionIdToColor } from "@/lib/utils";
+
+import UserAvatar from "./user-avatar";
+
+const MAX_SHOWN_USERS = 2;
 
 const Participants = () => {
+  const users = useOthers();
+
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
   return (
     <div className="absolute right-2 top-2 flex h-12 items-center rounded-md bg-white p-3 shadow-md">
-      List of users
+      <div className="gap-x2 flex">
+        {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              borderColor={connectionIdToColor(connectionId)}
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || "T"}
+            />
+          );
+        })}
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (You)`}
+            fallback={currentUser.info?.name?.[0]}
+          />
+        )}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_USERS} more`}
+            fallback={`+${users.length - MAX_SHOWN_USERS}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
