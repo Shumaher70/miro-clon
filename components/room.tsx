99915b81f815a8dec88a4ccece5aa244d1dc9e5d
Oleg Vrabiy <olegvraby@gmail.com>
2024-08-23 18:10:00 +0200

    add:room.tsx: 01.throttle 02.InitialPresence

--- components/room.tsx
+++ components/room.tsx
@@ -15,8 +15,13 @@ interface RoomProps {
 
 export function Room({ children, roomId, fallback }: RoomProps) {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
-      <RoomProvider id={roomId} initialPresence={{}}>
+    <LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
+      <RoomProvider
+        id={roomId}
+        initialPresence={{
+          cursor: null,
+        }}
+      >
         <ClientSideSuspense fallback={fallback}>{children}</ClientSideSuspense>
       </RoomProvider>
     </LiveblocksProvider>
