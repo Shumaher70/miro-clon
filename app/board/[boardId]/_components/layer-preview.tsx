3d4aef52452ea5b45d02e62ff5869ff899f9eca5
Oleg Vrabiy <olegvraby@gmail.com>
2024-09-06 17:08:25 +0200

    update:layer-preview.tsx: Additional condition for Note, Text, Ellipse displaying

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -2,9 +2,13 @@
 
 import { memo } from "react";
 
-import Rectangle from "./rectangle";
 import { LayerType } from "@/types/canvas";
 import { useStorage } from "@liveblocks/react/suspense";
+
+import Text from "./text";
+import Note from "./note";
+import Ellipse from "./Ellipse";
+import Rectangle from "./rectangle";
 
 interface LayerPreviewProps {
   id: string;
@@ -20,6 +24,33 @@ const LayerPreview = memo(
     }
 
     switch (layer.type) {
+      case LayerType.Note:
+        return (
+          <Note
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Ellipse:
+        return (
+          <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
       case LayerType.Rectangle:
         return (
           <Rectangle
